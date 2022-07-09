ba13acc9b8faacb2a642ed8743a63359d6d03c35
Cristi Cismas <cristicismas99@gmail.com>
2022-07-09 11:51:29 +0300

    Update error messages

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ mod args;
 
 use args::Args;
 use directories::ProjectDirs;
-use std::fs::{create_dir_all, read_to_string, read_dir, File};
+use std::fs::{create_dir_all, read_dir, read_to_string, File};
 use std::io::prelude::*;
 use std::path::Path;
-use std::env;
 
 fn main() {
     let args = Args::new();
@@ -23,7 +22,7 @@ fn main() {
 }
 
 fn display_available_commands() {
-    let commands = vec!["help", "folder", "list", "{env_name}"];
+    let commands = vec!["help", "folder", "list", "[env_name]"];
     println!("\nUsage: envch [COMMAND | ENV_NAME] [FOLDER_NAME]\n\n");
     println!("Available commands: \n");
 
@@ -32,7 +31,7 @@ fn display_available_commands() {
             "help" => println!("help - Displays usage info and available commands.\n"),
             "folder" => println!("folder - Sets the source folder of all the .env files.\n"),
             "list" => println!("list - Lists all available .env files.\n"),
-            "{env_name}" => println!("The name of the file in the selected folder. To view the selected folder type `envch list`\n"),
+            "[env_name]" => println!("[env_name] - The name of the file in the selected folder. To view the selected folder type `envch list`\n"),
             // Impossible case.
             _ => ()
         }
@@ -44,6 +43,8 @@ fn set_folder(name_reference: &Option<String>) {
     if let Some(data_directory) = ProjectDirs::from("", "", "envchanger") {
         init_folder_if_not_existent(data_directory.config_dir());
         create_config_file(data_directory.config_dir(), name_reference);
+
+        println!("Folder set to: {:?}", name_reference.as_ref().unwrap());
     } else {
         equit!("Cannot find a data directory for your current operating system.");
     }
@@ -81,8 +82,11 @@ fn list_folder_contents() {
         let config_dir = data_directory.config_dir().join("folder");
 
         let folder_name = read_to_string(config_dir).expect("You need to set the folder where your .env files reside before using `envch list`.\nTo do that please use `envch folder {folder_name}` and make sure this folder exists.");
-        
-        let environments = read_dir(folder_name).unwrap();
+
+        let environments = match read_dir(folder_name) {
+            Ok(envs) => envs,
+            Err(_) => equit!("Cannot read selected folder. Please make sure the folder is set (with `chenv folder [folder_name]`), and that the folder actually exists."),
+        };
 
         println!("Here are the environment which you can import into your current folder:\n");
 
@@ -90,7 +94,7 @@ fn list_folder_contents() {
             println!("{}", path.unwrap().file_name().to_str().unwrap())
         }
 
-        println!("\nTo import any of these .env files, simply go to your target directory and type `envch {{environment}}`.\n");
+        println!("\nTo import any of these .env files, simply go to your target directory and type `envch [environment]`.\n");
     } else {
         equit!("Cannot find a data directory for your current operating system.");
     }
